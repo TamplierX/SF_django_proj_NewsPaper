4386eef7f2a44a2d93c20051d31b693edca1c952
TamplierX <163639339+TamplierX@users.noreply.github.com>
2024-06-17 17:02:04 +0300

    Итоговое задание D2
    1. Cоздании приложения News Portal
    2. Cоздана модель Author
    3. Cоздана модель Category
    4. Cоздана модель Post
    5. Cоздана модель PostCategory
    6. Cоздана модель Comment
    7. Реализованы методы:
    1) Методы like() и dislike() в моделях Comment и Post, которые увеличивают/уменьшают рейтинг на единицу.
    2) Метод preview() модели Post, который возвращает начало статьи (предварительный просмотр) длиной 124 символа и добавляет многоточие в конце.
    3) Метод update_rating() модели Author, который обновляет рейтинг текущего автора.
    + Файл, в котором написаны список всех команд, запускаемых в Django shell.

---
+++ SkillFactory_proj_NewsPaper/NewsPaper/News/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+
+from .resources import CATEGORY_TYPE
+
+
+class Author(models.Model):
+    author_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    author_rating = models.FloatField(default=0.0)
+
+    def update_rating(self):
+        post_rat = self.post_set.aggregate(post_rating=Sum('content_rating'))
+        p_rat = 0
+        p_rat += post_rat.get('post_rating')
+
+        comment_rat = self.author_user.comment_set.aggregate(comment_rating=Sum('comment_rating'))
+        c_rat = 0
+        c_rat += comment_rat.get('comment_rating')
+
+        self.author_rating = p_rat * 3 + c_rat
+        self.save()
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=64, unique=True)
+
+
+class Post(models.Model):
+    post_author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content_category = models.CharField(max_length=2, choices=CATEGORY_TYPE, default="NE")
+    date_create = models.DateTimeField(auto_now_add=True)
+    post_category = models.ManyToManyField(Category, through='PostCategory')
+    content_title = models.CharField(max_length=255)
+    content_text = models.TextField()
+    content_rating = models.FloatField(default=0.0)
+
+    def like(self):
+        self.content_rating += 1
+        self.save()
+
+    def dislike(self):
+        self.content_rating -= 1
+        self.save()
+
+    def preview(self):
+        return f'{self.content_text[0:123]}...'
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    comment_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    date_create = models.DateTimeField(auto_now_add=True)
+    comment_rating = models.FloatField(default=0.0)
+
+    def like(self):
+        self.comment_rating += 1
+        self.save()
+
+    def dislike(self):
+        self.comment_rating -= 1
+        self.save()
---
+++ SkillFactory_proj_NewsPaper/NewsPaper/News/resources.py
@@ -0,0 +1,7 @@
+news = "NE"
+article = "AR"
+
+CATEGORY_TYPE = [
+    (news, "Новости"),
+    (article, "Статьи")
+]
